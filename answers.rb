547e1cf06c0bfc1eebb070b9f0d3096dec860e9c
Steven G. Harms <steven.harms@flatironschool.com>
2018-11-14 17:44:05 -0500

    Clarify prompts

--- answers.rb
+++ answers.rb
@@ -4,33 +4,30 @@ require_relative "./lib/card.rb"
 require_relative "./lib/table.rb"
 
 def name_of_the_game
-# Return the name of the game as a String
+  # Based on the rules, what game are you playing?
+  # Return its name, as a string, from this method.
 
 end
 
 def what_code_is_required_to_play_the_game
-# Return the single expression needed to start the game
-
+  # Type the code you would use to start the game.
+  # This should be a Ruby expression
 end
 
 def which_method_gets_keyed_user_input
-# Return the String of name of the instance method that gets user input
-
+  # Return a String that is the name of the instance method that gets user input
 end
 
 def which_methods_only_handle_display
-# Return an Array of String method names for all instance methods that strictly handle outputing content
-
+  # Find the method names for all instance methods that directly output content.
+  # Return an these methods' names as an Array of Strings
 end
 
-
 def which_method_creates_cards
-# Return the String name of the instance method that returns cards as a 'deck'
-
+  # Return a String that is the name of the instance method that returns cards as a 'deck'
 end
 
 def double_deck_game
-# Lets say we wanted to use two decks of cards
-# Return the String name of the method that we would modify to play a double deck game
-
+  # Suppose we wanted to use two decks of cards
+  # Return as a String the name of the method that we would modify to play a double deck game
 end
